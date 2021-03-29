9ae1963c37a27533f82e245e276b00b25965ce92
unknown <deekshiavru@gmail.com>
2021-03-29 10:56:22 +0530

    adding Project

---
+++ AzureDatabaseProject/Migrations/20210329051319_AzureDatabaseProject.Models.Movie.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureDatabaseProject.Migrations
+{
+    public partial class AzureDatabaseProjectModelsMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Duration = table.Column<float>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Duration", "Name" },
+                values: new object[] { 1, "Action movie", 120f, "Robert" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ AzureDatabaseProject/Models/ShowContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDatabaseProject.Models
+{
+    public class ShowContext:DbContext
+    {
+        public ShowContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            { Id = 1, Name = "Robert", Description = "Action movie", Duration = 120.0f });
+        }
+    }
+}
